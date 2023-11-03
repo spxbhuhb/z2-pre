4b7e5878b8f8797124eb4ace17f1ccfa2b81c2b3
Tóth István Zoltán <tiz@simplexion.hu>
2023-11-03 12:43:50 +0100

    long convenience

--- z2-browser/src/jsMain/kotlin/hu/simplexion/z2/browser/field/stereotype/LongField.kt
+++ z2-browser/src/jsMain/kotlin/hu/simplexion/z2/browser/field/stereotype/LongField.kt
@@ -4,6 +4,8 @@ import hu.simplexion.z2.browser.css.textAlignEnd
 import hu.simplexion.z2.browser.css.w304
 import hu.simplexion.z2.browser.field.FieldState
 import hu.simplexion.z2.browser.html.Z2
+import hu.simplexion.z2.browser.html.onBlur
+import hu.simplexion.z2.browser.html.onFocus
 import hu.simplexion.z2.browser.material.textfield.AbstractField
 import hu.simplexion.z2.browser.material.textfield.FieldConfig
 import kotlinx.dom.addClass
@@ -17,6 +19,19 @@ class LongField(
 ) {
     override fun main(): LongField {
         super.main()
+
+        input.onFocus {
+            if (! state.touched && valueOrNull == 0L) {
+                inputElement.value = ""
+            }
+        }
+
+        input.onBlur {
+            if (! state.touched && valueOrNull == 0L) {
+                value = 0L // to refresh the field
+            }
+        }
+
         addClass(w304)
         inputElement.addClass(textAlignEnd)
         inputElement.type = "number"
