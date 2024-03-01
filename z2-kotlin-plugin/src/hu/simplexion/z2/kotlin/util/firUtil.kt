7d8b66b7c8210696d476e8969b53c99ae63d2e07
Tóth István Zoltán <tiz@simplexion.hu>
2024-03-01 08:03:38 +0100

    firUtil (left out of last commit)

--- z2-kotlin-plugin/src/hu/simplexion/z2/kotlin/util/firUtil.kt
+++ z2-kotlin-plugin/src/hu/simplexion/z2/kotlin/util/firUtil.kt
@@ -1,5 +1,8 @@
 package hu.simplexion.z2.kotlin.util
 
+import org.jetbrains.kotlin.GeneratedDeclarationKey
+import org.jetbrains.kotlin.fir.declarations.FirDeclarationOrigin
+import org.jetbrains.kotlin.fir.extensions.MemberGenerationContext
 import org.jetbrains.kotlin.fir.symbols.SymbolInternals
 import org.jetbrains.kotlin.fir.symbols.impl.FirClassSymbol
 import org.jetbrains.kotlin.text
@@ -13,3 +16,9 @@ fun FirClassSymbol<*>.superTypeContains(vararg names : String) : Boolean =
         .any { source ->
             names.any { it in source }
         }
+
+fun MemberGenerationContext?.isFromPlugin(key : GeneratedDeclarationKey): Boolean {
+    val owner = this?.owner ?: return false
+    val ownerKey = (owner.origin as? FirDeclarationOrigin.Plugin)?.key ?: return false
+    return ownerKey == key
+}
