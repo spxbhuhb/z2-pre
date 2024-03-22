b3e1d60f23e22c7a864151a438ec6a8e31131d7b
Tóth István Zoltán <tiz@simplexion.hu>
2024-03-22 09:29:43 +0100

    table header height fix

--- z2-site/src/jsMain/kotlin/hu/simplexion/z2/browser/demo/table/tableDemo.kt
+++ z2-site/src/jsMain/kotlin/hu/simplexion/z2/browser/demo/table/tableDemo.kt
@@ -1,54 +1,59 @@
 package hu.simplexion.z2.browser.demo.table
 
 
+import hu.simplexion.z2.browser.css.backgroundTransparent
+import hu.simplexion.z2.browser.css.borderOutline
 import hu.simplexion.z2.browser.css.p0
 import hu.simplexion.z2.browser.demo.strings
 import hu.simplexion.z2.browser.html.Z2
+import hu.simplexion.z2.browser.immaterial.table.builders.TableBuilder
 import hu.simplexion.z2.browser.immaterial.table.table
-import hu.simplexion.z2.browser.layout.surfaceContainer
-import hu.simplexion.z2.browser.layout.surfaceContainerLow
+import hu.simplexion.z2.browser.layout.container
+import hu.simplexion.z2.browser.layout.surfaceContainerLowest
 import hu.simplexion.z2.browser.material.em
-import hu.simplexion.z2.browser.material.px
+
+fun <T> Z2.tablePage(builder: TableBuilder<T>.() -> Unit) =
+    surfaceContainerLowest(borderOutline) {
+        container(p0, backgroundTransparent, scroll = false) {
+            table(builder)
+        }
+    }
+
+class Row(
+    val v1: String,
+    val v2: Int
+)
 
 fun Z2.tableDemo() =
-    surfaceContainerLow(p0) {
-        style.paddingTop = 16.px
-        class Row(
-            val v1: String,
-            val v2: Int
-        )
+    tablePage<Row> {
 
         val data = (1..50).map {
             Row("s-$it", it)
         }
 
-        surfaceContainer(p0, scroll = false) {
-            style.background = "transparent"
-
-            table<Row> {
-                title {
-                   text = strings.tableTitle
-                }
-                search = true
-                add = true
-                export = true
-
-                rowId = { it.v1 }
-                query = { data }
-
-                column {
-                    label = strings.headerA
-                    render = { text { it.v1 } }
-                    comparator = { a, b -> a.v2.compareTo(b.v2) }
-                    initialSize = 10.em
-                }
-
-                column {
-                    label = strings.headerB
-                    render = { text { it.v2 } }
-                    comparator = { a, b -> a.v2.compareTo(b.v2) }
-                }
-
-            }
+        title {
+            text = strings.tableTitle
+            action(strings.addAccount) { }
         }
+
+        search = true
+        add = true
+        export = true
+
+        rowId = { it.v1 }
+        query = { data }
+
+        column {
+            label = strings.headerA
+            render = { text { it.v1 } }
+            comparator = { a, b -> a.v2.compareTo(b.v2) }
+            initialSize = 10.em
+        }
+
+        column {
+            label = strings.headerB
+            render = { text { it.v2 } }
+            comparator = { a, b -> a.v2.compareTo(b.v2) }
+        }
+
     }
