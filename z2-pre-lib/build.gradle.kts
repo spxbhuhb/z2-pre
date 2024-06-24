91a4ff0ca0dea0647e298398c0c8a85c22e18be0
Tóth István Zoltán <tiz@simplexion.hu>
2024-06-24 08:54:50 +0200

    2024.06.24

--- z2-pre-lib/build.gradle.kts
+++ z2-pre-lib/build.gradle.kts
@@ -3,7 +3,7 @@
  */
 plugins {
     kotlin("multiplatform") version "1.9.10"
-    id("hu.simplexion.z2.pre") version "2024.05.27"
+    id("hu.simplexion.z2.pre") version "2024.06.24"
     java
     signing
     `maven-publish`
